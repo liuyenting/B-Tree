193822bc3acd0d04420bb0ab7c2a832bbc685698
Andy Liu <windows.linux.mac@gmail.com>
2015-04-07 16:48:45 +0800

    Complete range find test application.

--- ad.cpp
+++ ad.cpp
@@ -8,6 +8,7 @@
 #include <streambuf>
 #include <istream>
 
+#ifdef MMF
 #include <unistd.h>
 #include <sys/mman.h>
 #include <cstdio>
@@ -15,18 +16,21 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#endif
 
 #include <list>
 
 #include "btree_multimap.h"
 
-#ifndef REMOTE
-//#define FILE_PATH "kddcup2012track2.txt"
-#define FILE_PATH "kdd_sample"
-#else
+#ifdef REMOTE
 #define FILE_PATH "/tmp2/KDDCup2012/track2/kddcup2012track2.txt"
+#elif LOCAL
+#define FILE_PATH "kddcup2012track2.txt"
+#else
+#define FILE_PATH "kdd_sample"
 #endif
 
+#ifdef MMF
 struct membuf : std::streambuf
 {
     membuf(char const* base, size_t size)
@@ -43,6 +47,7 @@ struct imemstream : virtual membuf, std::istream
     {
     }
 };
+#endif
 
 struct compound_key
 {
@@ -77,33 +82,33 @@ unsigned int parse_user_id(std::string &str, char delim)
     return static_cast<unsigned int>(std::stoi(str, 0, 10));
 }
 
-//void construct_tree(std::ifstream& ifs, stx::btree_multimap<unsigned int, std::streamoff, std::less<unsigned int> >&map)
-void construct_tree(imemstream& ims, stx::btree_multimap<unsigned int, std::streamoff, std::less<unsigned int> >&map)
+#ifndef MMF
+void construct_tree(std::ifstream& stream, stx::btree_multimap<unsigned int, std::streamoff, std::less<unsigned int> >&map)
+#else
+void construct_tree(imemstream& stream, stx::btree_multimap<unsigned int, std::streamoff, std::less<unsigned int> >&map)
+#endif
 {
     std::string new_line;
 
     std::cout << "Start constructing tree." << std::endl;
     
     int counter = 0;
+    std::ifstream::pos_type currentPos = 0;
 
-    ims.clear();
-    std::cout << "Init fpos: " << ims.tellg() << std::endl;
+    stream.clear();
+    std::cout << "Init fpos: " << stream.tellg() << std::endl;
 
-    while(!ims.eof())
+    while(!stream.eof())
     {
-        std::getline(ims, new_line);
-        //std::cout << "New line: " << new_line << std::endl;
+        // Get the pos_type of from the head of current line
+        currentPos = stream.tellg();
+        std::getline(stream, new_line);
         
         // Skip blank line
         if(new_line.length() == 0)
             continue;
 
-        /*
-        ims.clear();
-        map.insert(std::pair<unsigned int, std::streamoff>(parse_user_id(new_line, ' '), ims.tellg()));
-        ims.clear();
-        */
-        std::cout << "Current fpos: " << ims.tellg() << std::endl;
+        map.insert(std::pair<unsigned int, std::streamoff>(parse_user_id(new_line, '\t'), currentPos));
         
         counter++;
         
@@ -118,6 +123,7 @@ void construct_tree(imemstream& ims, stx::btree_multimap<unsigned int, std::stre
     std::cout << "Insertion complete!" << std::endl;
 }
 
+#ifdef MMF
 size_t getFileSize(const std::string& file_path)
 {
     struct stat st;
@@ -125,9 +131,11 @@ size_t getFileSize(const std::string& file_path)
         return 0;
     return st.st_size;
 }
+#endif
 
 int main()
 {
+    #ifdef MMF
     int fd = -1;
     void *data = NULL;
 
@@ -138,22 +146,26 @@ int main()
 
     size_t fileSize = getFileSize(FILE_PATH);
     // Map the file into memory
-    data = mmap((caddr_t)0, fileSize, PROT_READ, MAP_SHARED, fd, 0);
-    if(!data)
+    data = mmap((caddr_t)0, fileSize, PROT_READ, MAP_PRIVATE, fd, 0);
+    if(data == MAP_FAILED)
         throw "main(): Fail to map the file into memory.";
     else
         std::cout << "File mapped." << std::endl;
     
     imemstream ims(reinterpret_cast<char*>(data), fileSize);
-
-    //std::ifstream ifs(FILE_PATH, std::ifstream::in);
+    #else
+    std::ifstream ifs(FILE_PATH, std::ifstream::in);
+    #endif
 
     stx::btree_multimap<unsigned int, std::streamoff, std::less<unsigned int> > BTreeMap;
     
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
-    //construct_tree(ifs, BTreeMap);
+    #ifndef MMF
+    construct_tree(ifs, BTreeMap);
+    #else
     construct_tree(ims, BTreeMap);
+    #endif
     /*
     BTreeMap.insert(std::pair<unsigned int, std::streamoff>(0,0));
     BTreeMap.insert(std::pair<unsigned int, std::streamoff>(0,1));
@@ -174,30 +186,41 @@ int main()
     std::list<std::streamoff> lst;
 
     for(stx::btree_multimap<unsigned int, std::streamoff, std::less<unsigned int> >::iterator it = range.first; it != range.second; ++it)
-    {
         lst.push_back(it->second);
-    }
 
-
+    // Wipe the position flag
+    #ifdef MMF
+    ims.clear();
+    #else
+    ifs.clear();
+    #endif
 
     std::cout << "Found (fpos) {" << std::endl;
     for (auto it = lst.begin(); it != lst.end(); ++it)
     {
-        ims.clear();
-
         std::string buffer;
+
+        std::cout << "Seeking at: " << *it << std::endl;
+
+        #ifdef MMF
         ims.seekg(*it, ims.beg);
-        
         std::getline(ims, buffer);
+        #else
+        ifs.seekg(*it, ifs.beg);
+        std::cout << "Current fpos: " << ifs.tellg() << std::endl;
+        std::getline(ifs, buffer);
+        #endif
 
         std::cout << buffer << std::endl;
     }
     std::cout << " }" << std::endl;
 
+    #ifdef MMF
     // Unmap the file
     if(munmap(data, fileSize) == -1)
         throw "main(): Fail to un-mapping the file.";
     close(fd);
+    #endif
 
     return 0;
 }
